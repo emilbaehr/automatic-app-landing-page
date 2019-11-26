2b15c61a2191e6c6058d46d9a0d8f1878f9f7447
Guille <guillermogar@gmail.com>
2019-11-26 18:30:21 +0100

    fix
    Index, page name, favicon, signup

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 "use strict";
 
+ $(window).on('load', function () {
+	//------------------------------------------------------------------------
+	//						PRELOADER SCRIPT
+	//------------------------------------------------------------------------
+	$("#preloader").delay(400).fadeOut("slow");
+	$("#preloader .clock").fadeOut();
+});
+
 window.addEventListener('load', function() {
 
 
